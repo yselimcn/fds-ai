cc93c016ee4b7d0535a977f030121ed8aef87f68
Yavuz Selim Can <yavuzselimcan@proton.me>
2025-09-10 16:05:19 +0300

    initial commit:

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+        <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-sans sm:p-20">
+            <main className="row-start-2 flex flex-col items-center gap-[32px] sm:items-start">
+                test
+            </main>
+        </div>
+    )
+}
