8e81bf14d43cee8d4b5450a06b3f3a2055b77959
Yavuz Selim Can <yavuzselimcan@proton.me>
2025-09-16 12:04:04 +0300

    introduce theme-provider, theme-toggle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,14 @@
 import { getDictionary } from '@/lib/dictionary'
+import { ModeToggle } from '@/components/theme-toggle'
 
 export default async function Home() {
     const dict = await getDictionary()
 
     return (
         <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-sans sm:p-20">
-            <main className="row-start-2 flex flex-col items-center gap-[32px] sm:items-start">
-                <h1 className="text-3xl font-bold">{dict.page.home.title}</h1>
-                <p className="text-muted-foreground text-lg">
-                    {dict.page.home.description}
-                </p>
-            </main>
+            {dict.page.home.description}
+
+            <ModeToggle />
         </div>
     )
 }
